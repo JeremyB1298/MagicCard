cea5890d951da3a3976f2d5626146a95d0116efe
Bailly <jeremy.bailly@hotmail.com>
2018-10-15 16:03:38 +0200

    7

--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,3 @@
 <?php
 
-$app->get('/inscription','App\Users\Controller\IndexController::addInscriptionAction')->bind('users.addInscription');
+$app->post('/inscription','App\Users\Controller\IndexController::addInscriptionAction')->bind('users.addInscription');
