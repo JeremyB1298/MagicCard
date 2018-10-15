3ca117719289a0f2a795a5d4e6039139cc0d1494
ThomasP <trolldu25@live.fr>
2018-10-15 14:14:00 +0200

    Config

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,10 @@
 <?php
 
-$app->get('/users/list', 'App\Users\Controller\IndexController::listAction')->bind('users.list');
-$app->get('/users/edit/{id}', 'App\Users\Controller\IndexController::editAction')->bind('users.edit');
-$app->get('/users/new', 'App\Users\Controller\IndexController::newAction')->bind('users.new');
-$app->post('/users/delete/{id}', 'App\Users\Controller\IndexController::deleteAction')->bind('users.delete');
-$app->post('/users/save', 'App\Users\Controller\IndexController::saveAction')->bind('users.save');
+// $app->get('/users/list', 'App\Users\Controller\IndexController::listAction')->bind('users.list');
+// $app->get('/users/edit/{id}', 'App\Users\Controller\IndexController::editAction')->bind('users.edit');
+// $app->get('/users/new', 'App\Users\Controller\IndexController::newAction')->bind('users.new');
+// $app->post('/users/delete/{id}', 'App\Users\Controller\IndexController::deleteAction')->bind('users.delete');
+// $app->post('/users/save', 'App\Users\Controller\IndexController::saveAction')->bind('users.save');
+
+
+$app->get('users/connexion', 'App\App\Users\Controller\ConnexionController::listAction')->bind('users.connexion');
